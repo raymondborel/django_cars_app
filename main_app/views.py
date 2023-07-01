453bf743262c096c73d9c22786e82c2eff5139b4
raymondborel <rborel852@gmail.com>
2023-07-01 08:07:50 -0700

    Added About Route

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,6 @@ from django.views.generic.base import TemplateView
 # Here we will be creating a class called Home and extending it from the View class
 class Home(TemplateView):
     template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
