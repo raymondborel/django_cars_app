ffa16f163c179178a83e77d7433c3cf43bd5264b
raymondborel <rborel852@gmail.com>
2023-07-03 12:55:48 -0700

    Added Models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Make(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    country = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
