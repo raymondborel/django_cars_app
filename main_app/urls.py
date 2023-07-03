913222f7c6246f8e5757478bb0d9511021ac8267
raymondborel <rborel852@gmail.com>
2023-07-03 12:45:51 -0700

    Added title

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('makes/', views.MakeList.as_view(), name="make_list"),
 ]
