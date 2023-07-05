8c505649a05548b05030080b85082b1a17a8de81
raymondborel <rborel852@gmail.com>
2023-07-04 20:10:23 -0700

    Changed carmodel model/ added image

--- main_app/models.py
+++ main_app/models.py
@@ -21,6 +21,7 @@ class CarModel(models.Model):
     name = models.CharField(max_length=150)
     price = models.IntegerField(default=0)
     make = models.ForeignKey(Make, on_delete=models.CASCADE, related_name="carmodels")
+    image = models.CharField(max_length=300)
 
     def __str__(self):
         return self.name
