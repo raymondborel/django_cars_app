2c1298dd400049054113958625f088aae4fad8de
raymondborel <rborel852@gmail.com>
2023-07-04 17:47:05 -0700

    Created Car Model Model

--- main_app/models.py
+++ main_app/models.py
@@ -7,8 +7,20 @@ class Make(models.Model):
     country = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
         return "Make: " + self.name
 
     class Meta:
         ordering = ['name']
+
+# below Artist Model
+
+class CarModel(models.Model):
+
+    name = models.CharField(max_length=150)
+    price = models.IntegerField(default=0)
+    make = models.ForeignKey(Make, on_delete=models.CASCADE, related_name="carmodels")
+
+    def __str__(self):
+        return self.title
